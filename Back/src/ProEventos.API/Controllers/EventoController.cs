0c4d5058c0eb9f347dacda7698f47ea2bffb898d
Ronei-Cruz <roneidiegocruz@gmail.com>
2023-03-16 16:11:12 -0300

    Modificação Program, add Controllers e Models

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento();
+        }
+    }
+}
