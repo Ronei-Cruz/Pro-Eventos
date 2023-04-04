26739588eab125684612c0cdeadf632fecfc13e5
Ronei-Cruz <roneidiegocruz@gmail.com>
2023-04-04 14:09:31 -0300

    Add UseCors

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -26,6 +26,7 @@ namespace ProEventos.API
                 context => context.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -47,6 +48,8 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
