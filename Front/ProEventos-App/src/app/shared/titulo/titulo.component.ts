82a5bfdfd924ddf1b784477e5a225f67c159b083
Ronei-Cruz <roneidiegocruz@gmail.com>
2023-04-13 16:57:57 -0300

    Padronizando views

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,13 +3,19 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
   @Input() titulo: string;
+  @Input() subtitulo: string;
+  @Input() iconClass: string;
+  @Input() botaoListar: boolean;
 
   constructor() {
-    this.titulo = ''
+    this.titulo = '';
+    this.subtitulo = 'Desde 2023'
+    this.iconClass = '';
+    this.botaoListar = false;
   }
 
   ngOnInit() : void {
